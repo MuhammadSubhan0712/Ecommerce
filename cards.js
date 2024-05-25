3b0070f50646620094e91abeb8ac3efcba8283c5
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-05-26 02:15:52 +0500

    Assigned

--- cards.js
+++ cards.js
@@ -5,7 +5,7 @@
 
 
 
-
+`
 <div class="card" style="width: 18rem;">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
@@ -13,5 +13,7 @@
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="card-link">Card link</a>
     <a href="#" class="card-link">Another link</a>
+    <button class = "btn btn-primary" >  <button/>
   </div>
 </div>
+`
