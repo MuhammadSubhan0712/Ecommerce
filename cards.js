c91560bf6094896312bb7d50e194fd58b7f67bfe
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-05-26 17:12:34 +0500

    Assigned

--- cards.js
+++ cards.js
@@ -6,7 +6,7 @@
 
 
 `
-<div class="card" style="width: 18rem;">
+
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
